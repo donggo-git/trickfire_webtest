137800e2105072f808163b1a1765570dcd2bfe50
agrach060 <anna.gracheva060@gmail.com>
2023-05-20 01:00:52 -0700

    created components

--- src/components/about_us.vue
+++ src/components/about_us.vue
@@ -1,17 +1,19 @@
 <script lang="ts">
+import aboutUs from '../components/row_blocks.vue';
 import '../scss/AboutUs.scss';
 import { aboutUsSectionData } from '../data/about_us_data.ts';
 
   export default {
     name: 'MyComponent',
+    components: {
+        aboutUs
+    },
     data() {
       return {
         aboutUsSectionData: aboutUsSectionData
       }
     }
   }
-
-
 
 </script>
 
@@ -39,225 +41,22 @@ import { aboutUsSectionData } from '../data/about_us_data.ts';
 
         <section>
             <div class="rows">
-<!--SOFTWARE-->            
-                <div class="image__container">
-                    <div class="image__left">
-                        <picture>
-                            <source srcset="../assets/images/unslanted_software.png" media="(max-width: 1000px)">
-                            <source srcset="../assets/images/slanted_software.png">
-                            <img src="../assets/images/slanted_software.png" >
-                        </picture>
-                    </div>
 
-                        
-                    <div class="words-on-background" >
-                        <img src="../assets/images/fixed_index_header_left.png" />
-
-                        <div class="text-container" >
-                                <span class="fs-2">SOFTWARE TEAM</span>
-                                <p>{{ aboutUsSectionData[1].content }}</p> 
-                        </div>
-                    </div>      
-                </div>
-
-                <div class="image__container responsive">
-                    <div class="first-block responsive"> 
-                        <div class="image__left responsive">
-                            <img src="../assets/images/unslanted_software.png">
-                        </div>
-                        <div class="words-on-background responsive">
-                            <span class="fs-2">SOFTWARE TEAM</span>
-                        </div>
-                    </div>
-
-                    <div class="second-block responsive">    
-                        <div class="image__left responsive">
-                            <img src="../assets/images/unslanted_software.png">
-                        </div>
-
-                        <div class="span__container responsive">
-                            <p>{{ aboutUsSectionData[1].content }}</p>
-                        </div>
-                    </div>
-                </div>
-
-
-<!--MECHANICAL-->
-                <div class="image__container">
-                    <div class="image__left">
-                        <picture>
-                            <source srcset="../assets/images/unslanted_drilling_full.png" media="(max-width: 1000px)">
-                            <source srcset="../assets/images/slanted_drilling_right.png">
-                            <img src="../assets/images/slanted_drilling_right.png" >
-                        </picture>
-                    </div>
-
-                    <div class="words-on-background" >
-                        <img src="../assets/images/fixed_index_header_left.png" />
-
-                        <div class="text-container" >
-                            <span class="fs-2">MECHANICAL TEAM</span>
-                            <p>{{ aboutUsSectionData[0].content }}</p> 
-                        </div>
-                    </div> 
-                </div>
-
-                <div class="image__container responsive">
-                    <div class="first-block responsive"> 
-                        <div class="image__left responsive">
-                            <img src="../assets/images/unslanted_drilling_full.png">
-                        </div>
-                        <div class="words-on-background responsive">
-                            <span class="fs-2">MECHANICAL TEAM</span>
-                        </div>
-                    </div>
-
-                    <div class="second-block responsive">    
-                        <div class="image__left responsive">
-                            <img src="../assets/images/unslanted_drilling_full.png">
-                        </div>
-
-                        <div class="span__container responsive">
-                            <p>{{ aboutUsSectionData[0].content }}</p>
-                        </div>
-                    </div>
-                </div>
-<!--ELECTRICAL-->
-                <div class="image__container">
-                    <div class="image__left">
-                        <picture>
-                            <source srcset="../assets/images/unslanted_electrical.png" media="(max-width: 1000px)">
-                            <source srcset="../assets/images/slanted_electrical.png">
-                            <img src="../assets/images/slanted_electrical.png" >
-                        </picture>
-                    </div>
-
-                    <div class="words-on-background" >
-                        <img src="../assets/images/fixed_index_header_left.png" />
-
-                        <div class="text-container" >
-                            <span class="fs-2">ELECTRICAL TEAM</span>
-                            <p>{{ aboutUsSectionData[2].content }}</p> 
-                        </div>
-                    </div>                    
-                </div>
-
-                <div class="image__container responsive">
-                    <div class="first-block responsive"> 
-                        <div class="image__left responsive">
-                            <img src="../assets/images/unslanted_electrical.png">
-                        </div>
-                        <div class="words-on-background responsive">
-                            <span class="fs-2">ELECTRICAL TEAM</span>
-                        </div>
-                    </div>
-
-                    <div class="second-block responsive">    
-                        <div class="image__left responsive">
-                            <img src="../assets/images/unslanted_electrical.png">
-                        </div>
-
-                        <div class="span__container responsive">
-                            <p>{{ aboutUsSectionData[2].content }}</p>
-                        </div>
-                    </div>
-                </div>
-<!--FINANCE-->
-                <div class="image__container">
-                    <div class="image__left">
-                        <picture>
-                            <source srcset="../assets/images/unslanted_finance.png" media="(max-width: 1000px)">
-                            <source srcset="../assets/images/slanted_finance.png">
-                            <img src="../assets/images/slanted_finance.png" >
-                        </picture>
-                    </div>
-
-                    <div class="words-on-background" >
-                        <img src="../assets/images/fixed_index_header_left.png" />
-
-                        <div class="text-container" >
-                            <span class="fs-2">FINANCE TEAM</span>
-                            <p>{{ aboutUsSectionData[3].content }}</p> 
-                        </div>
-                    </div>
-                </div>
-
-                <div class="image__container responsive">
-                    <div class="first-block responsive"> 
-                        <div class="image__left responsive">
-                            <img src="../assets/images/unslanted_finance.png">
-                        </div>
-                        <div class="words-on-background responsive">
-                            <span class="fs-2">FINANCE TEAM</span>
-                        </div>
-                    </div>
-
-                    <div class="second-block responsive">    
-                        <div class="image__left responsive">
-                            <img src="../assets/images/unslanted_finance.png">
-                        </div>
-
-                        <div class="span__container responsive">
-                            <p>{{ aboutUsSectionData[3].content }}</p>
-                        </div>
-                    </div>
-                </div>
-<!--OUTREACH-->
-                <div class="image__container">
-                    <div class="image__left">
-                        <picture>
-                            <source srcset="../assets/images/unslanted_outreach_2016.jpg" media="(max-width: 1000px)">
-                            <source srcset="../assets/images/slanted_outreach_2016.png">
-                            <img src="../assets/images/slanted_outreach_2016.png" >
-                        </picture>
-                    </div>
-
-                    <div class="words-on-background" >
-                        <img src="../assets/images/fixed_index_header_left.png" />
-
-                        <div class="text-container" >
-                            <span class="fs-2">OUTREACH TEAM</span>
-                            <p>
-                                {{ aboutUsSectionData[4].content }}
-                                <ul>
-                                    <li>Recruiting new members</li>
-                                    <li>Reaching out to local industry</li>
-                                    <li>Handling public relations</li>
-                                    <li>Keeping our social media up to date</li>
-                                </ul>
-                            </p> 
-                        </div>
-                    </div>
-                </div>
-
-                <div class="image__container responsive">
-                    <div class="first-block responsive"> 
-                        <div class="image__left responsive">
-                            <img src="../assets/images/unslanted_outreach_2016.jpg">
-                        </div>
-                        <div class="words-on-background responsive">
-                            <span class="fs-2">OUTREACH TEAM</span>
-                        </div>
-                    </div>
-
-                    <div class="second-block responsive">    
-                        <div class="image__left responsive">
-                            <img src="../assets/images/unslanted_outreach_2016.jpg">
-                        </div>
-
-                        <div class="span__container responsive">
-                            <p>
-                                {{ aboutUsSectionData[4].content }}
-                                <ul>
-                                    <li>Recruiting new members</li>
-                                    <li>Reaching out to local industry</li>
-                                    <li>Handling public relations</li>
-                                    <li>Keeping our social media up to date</li>
-                                </ul>
-                            </p>
-                        </div>
-                    </div>
-                </div>
+        <!--SOFTWARE--> 
+                <aboutUs :image__left ="aboutUsSectionData[0].image" :title = "aboutUsSectionData[0].title" 
+                    :text = "aboutUsSectionData[0].content" :image__left__responsive = "aboutUsSectionData[0].imageResponsive"/>
+        <!--MECHANICAL-->
+                <aboutUs :image__left ="aboutUsSectionData[1].image" :title = "aboutUsSectionData[1].title" 
+                    :text = "aboutUsSectionData[1].content" :image__left__responsive = "aboutUsSectionData[1].imageResponsive"/>
+        <!--ELECTRICAL-->
+                <aboutUs :image__left ="aboutUsSectionData[2].image" :title = "aboutUsSectionData[2].title" 
+                    :text = "aboutUsSectionData[2].content" :image__left__responsive = "aboutUsSectionData[2].imageResponsive"/>
+        <!--FINANCE-->
+                <aboutUs :image__left ="aboutUsSectionData[3].image" :title = "aboutUsSectionData[3].title" 
+                    :text = "aboutUsSectionData[3].content" :image__left__responsive = "aboutUsSectionData[3].imageResponsive"/>
+        <!--OUTREACH-->
+                <aboutUs :image__left ="aboutUsSectionData[4].image" :title = "aboutUsSectionData[4].title" 
+                    :text = "aboutUsSectionData[4].content" :image__left__responsive = "aboutUsSectionData[4].imageResponsive"/>
             </div>
         </section> 
     </div>
---
+++ src/components/row_blocks.vue
@@ -0,0 +1,90 @@
+<script>
+import { aboutUsSectionData } from '../data/about_us_data.ts';
+import '../scss/AboutUs.scss';
+
+    export default {
+        name: "aboutUs",
+        props: {
+            image__left: String,
+            title: String,
+            text: String,
+            image__left__responsive: String,
+        },
+        computed: {
+            // bullet points in the last block
+            isFifthBlock() {
+                return this.title === aboutUsSectionData[4].title;
+            },
+            listItems() {
+                // Define the bullet point items for the fifth block
+                return this.text.split("\n•");
+            },
+        },
+    };
+</script>
+
+<template>
+    <div class="image__container">
+        <div class="image__left">
+            <img :src="image__left"> 
+        </div>
+
+        <div class="words-on-background" >
+            <img src="../assets/images/fixed_index_header_left.png" />
+
+            <div class="text-container" >
+                    <span class="fs-2">{{ title }}</span>
+                    <template v-if="isFifthBlock">
+                        <p>{{listItems[0]}}</p>
+                        <ul>
+                            <li v-for="(item, index) in listItems.slice(1)" :key="index" :class="{ 'bullet-point': index !== 0 && item.startsWith('• ') }">
+                                {{ item.startsWith('• ') ? item.substring(2) : item }}
+                            </li>
+                        </ul>
+                    </template>
+                    <template v-else>
+                        <p>{{ text }}</p>
+                    </template>
+            </div>
+        </div>      
+    </div>
+
+    <div class="image__container responsive">
+        <div class="first-block responsive"> 
+            <div class="image__left responsive">
+                <img :src="image__left__responsive">
+            </div>
+
+            <div class="words-on-background responsive">
+                <span class="fs-2">{{ title }}</span>
+            </div>
+        </div>
+
+        <div class="second-block responsive">    
+            <div class="image__left responsive">
+                <img :src="image__left__responsive">
+            </div>
+
+            <div class="span__container responsive">
+                <template v-if="isFifthBlock">
+                    <p>{{listItems[0]}}</p>
+                    <ul>
+                        <li v-for="(item, index) in listItems.slice(1)" :key="index" :class="{ 'bullet-point': index !== 0 && item.startsWith('• ') }">
+                            {{ item.startsWith('• ') ? item.substring(2) : item }}
+                        </li>
+                    </ul>
+                    </template>
+                    <template v-else>
+                        <p>{{ text }}</p>
+                    </template>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+
+
+
+
+
