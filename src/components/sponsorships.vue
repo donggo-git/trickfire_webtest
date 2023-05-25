3ba207ed83e39ac99ebb4cf3f11bd15bf5027fae
Dong <dongn12@uw.edu>
2023-05-24 22:13:33 -0700

    create header for sponsorship page

--- src/components/sponsorships.vue
+++ src/components/sponsorships.vue
@@ -1,4 +1,5 @@
 <script lang="ts">
+import '../scss/sponsorship.scss'
 export default {
 
     data() {
@@ -6,4 +7,26 @@ export default {
     }
 }
 </script>
-<template></template>
+
+<template>
+    <header class="sponsorship__header">
+        <div class="sponsorship__header__background">
+            <div class="d-flex flex-column justify-content-center align-items-center sponsorship__header__content">
+                <h1 class="m-0" style="color: white;">
+                    Sponsorship
+                </h1>
+                <p class="fs-5 my-3" style="color: white;">
+                    TrickFire relies on generous donations to purchase the necessary materials and equipment to build the
+                    rover and travel to competitions. Your donations will directly impact the development of our students.
+                    In-kind sponsorships are also a tax-favored solution to help our team get much-needed resources and
+                    tools to help construct the robot. In addition to donations, we seek industry mentors. Our team strives
+                    for professional quality work. As such, working with your organization’s professionals provides
+                    invaluable experience that helps us reach that goal while giving you a direct connection to candidate
+                    employees.
+                </p>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style lang="scss"></style>
