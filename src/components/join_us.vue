e7f5ca3c7c801b664330fcd5b4fc1d72bbc926e8
agrach060 <anna.gracheva060@gmail.com>
2023-05-28 10:28:10 -0700

    Completed Join Us page

--- src/components/join_us.vue
+++ src/components/join_us.vue
@@ -1,4 +1,6 @@
 <script lang="ts">
+import '../scss/JoinUs.scss';
+
 export default {
 
     data() {
@@ -7,4 +9,27 @@ export default {
 }
 
 </script>
-<template></template>
+<template>
+    <header id="join-us-header" class="header">
+        <div class="header__background">
+            <div class="header__content">
+                <h1>
+                    <p class="m-0 d-flex flex-column justify-content-center">
+                        <span class="fs-1" style="color: white;">WHY JOIN?</span>
+                    </p>
+                </h1>
+                <p class="fs-3 m-0" style="color: white;">
+                    TrickFire's mission is to give students the hand-on experience and
+                    skills in software, mechanical and electrical engineering, and business
+                    skills, which will prepare students for their resumes, and they will be
+                    able to talk about their work in TrickFire, while having fun along the way!
+                    No previous robotic experience is needed. <b>Deadline: Open until the end of summer</b>
+                </p>
+                <button class="button application"><a class="link_join_us"
+                        href="https://forms.office.com/Pages/ResponsePage.aspx?id=W9229i_wGkSZoBYqxQYL0i7wGfH_Ef9MlM3y37_kRLpUMEVVSDJTTFFOU0RNOEhNVVYyWUI2TjdOTyQlQCN0PWcu">Application
+                        Link</a>
+                </button>
+            </div>
+        </div>
+    </header>
+</template>
